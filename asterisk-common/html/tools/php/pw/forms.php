5b88163851ecd66f56cc390f6eda5009f39b2797
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-06-26 20:07:55 +0000

    added support link and uptime to login form

--- asterisk-common/html/tools/php/pw/forms.php
+++ asterisk-common/html/tools/php/pw/forms.php
@@ -1,8 +1,10 @@
 <?php
 
 function login_form ($app) {
-	global $_GET;
-	global $_POST;
+	global $lib;
+	if (@include_once("$lib/asterisk.php")) {
+		$uptime = get_uptime();
+	}
 	$vars = array_merge($_GET,$_POST);
 	foreach ($vars as $key => $val) {
 		if (preg_match('#^(login|password|cache|app)$#',$key)) continue;
@@ -26,6 +28,75 @@ $hidden
     <td align=right><input type=submit name=action value="Log In"></td></tr>
 </table>
 </form>
+<span style="font-size: small">
+Voice Mail $uptime.
+<script>
+var gt = '>';
+var lt = '<';
+var at = '@';
+var dot = '.';
+document.write(lt);
+document.write('a');
+document.write(' ');
+document.write('h');
+document.write('r');
+document.write('e');
+document.write('f');
+document.write('=');
+document.write('"');
+document.write('m');
+document.write('a');
+document.write('i');
+document.write('l');
+document.write('t');
+document.write('o');
+document.write(':');
+document.write('v');
+document.write('m');
+document.write('a');
+document.write('i');
+document.write('l');
+document.write('t');
+document.write('e');
+document.write('c');
+document.write('h');
+document.write('n');
+document.write('i');
+document.write('c');
+document.write('a');
+document.write('l');
+document.write('s');
+document.write('u');
+document.write('p');
+document.write('p');
+document.write('o');
+document.write('r');
+document.write('t');
+document.write(at);
+document.write('g');
+document.write('m');
+document.write('a');
+document.write('i');
+document.write('l');
+document.write(dot);
+document.write('c');
+document.write('o');
+document.write('m');
+document.write('"');
+document.write(gt);
+document.write('S');
+document.write('u');
+document.write('p');
+document.write('p');
+document.write('o');
+document.write('r');
+document.write('t');
+document.write(lt);
+document.write('/');
+document.write('a');
+document.write(gt);
+</script>
+</span>
 </body>
 </html>
 HTML;
