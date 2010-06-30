f108ef33567353137f04e1efc8649749ad274fc0
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-06-30 16:36:27 +0000

    added some extra stuff needed by cleanup.pl

--- trunk/asterisk-common/agi-bin/Lifeline/DB.pm
+++ trunk/asterisk-common/agi-bin/Lifeline/DB.pm
@@ -5,7 +5,7 @@ use Exporter;
 use DBI;
 our @ISA = qw/Exporter/;
 
-our @EXPORT = qw/$lldatafile $ldb/;
+our @EXPORT = qw/$lldatafile $ldb $llmsgdir $lleol ll_deleted_boxes/;
  
 our $dsn = "DBI:mysql:database=lifeline;host=localhost";
 our $username = 'll';
@@ -14,5 +14,20 @@ our $ldb = DBI->connect($dsn,$username,$password) or die DBI::errstr;
 END { $ldb->disconnect if defined $ldb; }
 
 our $lldatafile = "/usr/local/asterisk/Lifeline/users.csv";
+our $llmsgdir = "/usr/local/asterisk/lifeline-msgs";
+# end of life in days
+our $lleol = 90;
+
+sub ll_deleted_boxes {
+	my %delbox;
+	my $getq = "select box from boxes where status = 'deleted'";
+	my $get = $ldb->prepare($getq);
+	$get->execute or die $get->errstr;
+	while (my $row = $get->fetch) {
+		$delbox{$row->[0]} = 1;
+	}
+	$get->finish;
+	\%delbox;
+}
 
 1;
