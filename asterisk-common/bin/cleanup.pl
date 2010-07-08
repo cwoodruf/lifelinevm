91e2799be53fd2b7111e65e3e4d14d7c3ea63a07
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-07-08 23:17:08 +0000

    make updates from the backup servers more specific made cleanup more efficient and also able to remove message files that had already been marked for delete

--- asterisk-common/bin/cleanup.pl
+++ asterisk-common/bin/cleanup.pl
@@ -26,21 +26,25 @@ print scalar(keys %{$oldbox})," deleted boxes\n" if $opt{v};
 
 # now scan directories for stuff we should delete
 finddepth(\&markdelete, $llmsgdir);
-finddepth(\&purge, $llmsgdir);
 
 sub markdelete {
-	return unless $File::Find::name =~ m#(\d+)/(?:(greeting)\.gsm|messages/(\d+\.\d+)\.gsm)#;
-	my ($box,$stub) = ($1,$2);
-	return unless $oldbox->{$box};
-	print "old? $oldbox->{$box} - checking $box $stub $File::Find::name\n" if $opt{v};
-	move $_, "$stub.deleted.gsm" or die "$_ -> can't mark as deleted: $!";
-}
-
-sub purge {
-	return unless /\.deleted\.gsm$/;
-	my ($created) = ($1);
-	my $deleteme = (time - (stat)[9] > $lleol * 86400);
-	return unless $deleteme;
-	print "purge check $File::Find::name delete? $deleteme\n" if $opt{v};
+	# look for old messages and messages that have already been deleted (problem with rsyncing)
+	if ($File::Find::name =~ m#(\d+)/(?:(greeting)\.gsm|messages/(\d+\.\d+)\.gsm)#) {
+		my ($box,$grtstub,$msgstub) = ($1,$2,$3);
+		my $stub = $grtstub || $msgstub;
+		if ($oldbox->{$box}) {
+			print "old? $oldbox->{$box} - checking $box $stub $File::Find::name\n" if $opt{v};
+			move $_, "$stub.deleted.gsm" or die "$_ -> can't mark as deleted: $!";
+		} elsif (-f "$stub.deleted.gsm") {
+			print "markdelete removing $stub because its already deleted!\n" if $opt{v};
+			unlink $_;
+		}
+	# remove old messages that have been marked for deletion	
+	} elsif (/\.deleted\.gsm$/) {
+		my $deleteme = (time - (stat)[9] > $lleol * 86400);
+		return unless $deleteme;
+		print "purge check $File::Find::name delete? $deleteme\n" if $opt{v};
+		unlink $_;
+	}
 }
 
