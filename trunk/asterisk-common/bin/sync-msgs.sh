4f4fd994efc29d82f7671af7b7e0280fd8cdcf7a
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-07-08 23:17:08 +0000

    make updates from the backup servers more specific made cleanup more efficient and also able to remove message files that had already been marked for delete

--- trunk/asterisk-common/bin/sync-msgs.sh
+++ trunk/asterisk-common/bin/sync-msgs.sh
@@ -2,15 +2,20 @@
 # in the event of an outage the other servers will have messages on them
 # if this becomes routine we'd need to use the db to save message data
 msgdir=/usr/local/asterisk/lifeline-msgs/
+us=asterisk@lifelinevm.net
+findcmd="/usr/bin/find $msgdir -regex '.*\(\.[0-9]+\|greeting\)\.gsm$' -newer ~/lastupdated -print -exec /usr/bin/scp --preserve {} $us:{} \;"
+lastupdate="/bin/touch ~/lastupdated"
 
-echo sync-msgs: FROM local backup TO main
-/usr/bin/rsync -rcvt --times asterisk@192.168.1.44:$msgdir $msgdir
-echo sync-msgs: FROM remote backup TO main
-/usr/bin/rsync -rcvt --times asterisk@aifl.ath.cx:$msgdir $msgdir
+echo sync-msgs: FROM local backup TO main using ssh
+/usr/bin/ssh asterisk@192.168.1.44 "$findcmd"
+/usr/bin/ssh asterisk@192.168.1.44 "$lastupdate"
+
+echo sync-msgs: FROM remote backup TO main using ssh
+/usr/bin/ssh asterisk@aifl.ath.cx "$findcmd"
+/usr/bin/ssh asterisk@aifl.ath.cx "$lastupdate"
 
 echo sync-msgs: CLEANUP
 /usr/local/asterisk/bin/cleanup.pl -v
-/usr/bin/find $msgdir -name .deleted.gsm -exec /bin/rm {} \;
 
 echo sync-msgs: FROM main TO local backup with delete
 /usr/bin/rsync -rcvt --delete --times $msgdir asterisk@192.168.1.44:$msgdir
