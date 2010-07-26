ac3a55c625b8f8fa1193ca2576627bbd1a430c68
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-07-26 05:15:35 +0000

    reduced volume on listen and lastmsg prompts

--- trunk/telephony/dialogic/lifeline/sounds/replace.sh
+++ trunk/telephony/dialogic/lifeline/sounds/replace.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # these are especially loud prompts that should needed to be fixed
+cp ll-en-listen.gsm /usr/local/asterisk/var/lib/asterisk/sounds
+cp ll-en-listen.gsm ../../../../asterisk-common/sounds
+cp ll-en-lastmsg.gsm /usr/local/asterisk/var/lib/asterisk/sounds
+cp ll-en-lastmsg.gsm ../../../../asterisk-common/sounds
 cp ll-en-nomsgs.gsm /usr/local/asterisk/var/lib/asterisk/sounds
 cp ll-en-nomsgs.gsm ../../../../asterisk-common/sounds
 cp ll-en-recgrt.gsm /usr/local/asterisk/var/lib/asterisk/sounds
