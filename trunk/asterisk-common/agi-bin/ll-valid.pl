967c30e15991728cfab1b2455885102a64a939cf
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-08-06 15:21:04 +0000

    changed when a new paidto date gets made so that the caller has to log in needed to change what constitutes a valid paidto condition as well so box would not come up as invalid if there was no paidto date but there was an "add x months" status message.

--- trunk/asterisk-common/agi-bin/ll-valid.pl
+++ trunk/asterisk-common/agi-bin/ll-valid.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# purpose of this script is to load the greeting variable with the correct greeting file
+# purpose of this script is to check the paidto date / status before checking for the greeting
 use Lifeline;
 use strict;
 my $ll = Lifeline->init(); # use ->load if you need cached messages list
