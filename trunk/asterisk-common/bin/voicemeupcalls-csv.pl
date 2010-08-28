21fb2522f39fc848e291f8e78505699efb6674a2
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-08-28 09:15:46 +0000

    renamed voicemeupcalls.pl voicemeupcalls-xmlapi.pl and added voicemeupcalls-csv.pl to process the hand downloaded logs voicemeup's api only allows 25 calls to be downloaded making it not very useful

---
+++ trunk/asterisk-common/bin/voicemeupcalls-csv.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+# insert hand grabbed voicemeup logs see also ...-xmlapi.pl for the xml version
+use Lifeline::DB;
+use strict;
+
+# scan all files given: the first line in each file should be the field names
+my (@fields, $ins, $insq);
+while (<>) {
+	chomp;
+	unless (m/"/) {
+		@fields = split ",";
+		$insq = "replace into voicemeupcalls (unique_id,".(join ",", @fields).") ".
+				"values (?,".(join ",", map {"?"} @fields).")";
+		$ins = $ldb->prepare($insq);
+		next;
+	}
+	warn "no fields!\n$_" and next unless defined @fields;
+	my @values = map { s/^"//; s/"$//; $_ } split ",";
+	$ins->execute(@values) or die $ins->errstr.":\n$insq\n@values";
+}
