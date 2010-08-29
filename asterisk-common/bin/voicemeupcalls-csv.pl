5ebf495c8e0bd1090d71090e8637d5396716a6cb
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-08-29 06:00:21 +0000

    fixed missing field in voicemeupcalls-csv.pl

--- asterisk-common/bin/voicemeupcalls-csv.pl
+++ asterisk-common/bin/voicemeupcalls-csv.pl
@@ -15,6 +15,6 @@ while (<>) {
 		next;
 	}
 	warn "no fields!\n$_" and next unless defined @fields;
-	my @values = map { s/^"//; s/"$//; $_ } split ",";
-	$ins->execute(@values) or die $ins->errstr.":\n$insq\n@values";
+	my %data; @data{@fields} = my @values = map { s/^"//; s/"$//; $_ } split ",";
+	$ins->execute($data{call_hash},@values) or die $ins->errstr.":\n$insq\n@values";
 }
