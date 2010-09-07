998621107fcfdba7cd136255d55e00387911bd05
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-09-07 18:21:36 +0000

    fixed problem where hangup section of dial plan was not being recognized (eliminated that section) changed where box numbers entered were being logged to ll-valid from ll-greeting changed order of call even output to use callstart primarily

--- asterisk-common/agi-bin/ll-valid.pl
+++ asterisk-common/agi-bin/ll-valid.pl
@@ -4,6 +4,16 @@ use Lifeline;
 use strict;
 my $ll = Lifeline->init(); # use ->load if you need cached messages list
 my $paidto_cutoff = shift;
-if ($ll->valid and $ll->check_paidto($paidto_cutoff)) {
-	$ll->{agi}->set_priority(100);
-} 
+my $callerid = shift;
+if ($ll->valid) {
+	if ($ll->check_paidto($paidto_cutoff)) {
+		$ll->log_calls('ll-valid.pl','valid',$callerid);
+		$ll->{agi}->set_priority(100);
+	} else {
+		# never usually gets here
+		$ll->log_calls('ll-valid.pl','expired',$callerid);
+	}
+} else {
+	$ll->log_calls('ll-valid.pl','invalid',$callerid);
+}
+
