617eab008ded9899891491385a54f6b6bd588961
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-10-02 08:28:42 +0000

    added -f{file} option and -h options for gettweets.pl

--- trunk/asterisk-common/bin/gettweets.pl
+++ trunk/asterisk-common/bin/gettweets.pl
@@ -8,7 +8,17 @@ use Data::Dumper;
 use Date::Manip;
 use strict;
 my %opt;
-getopts('l:c:v',\%opt);
+getopts('l:c:f:vh',\%opt);
+my $options = <<TXT;
+gettweets.pl [-l{login}] [-c{count}] [-f{output file}] [-vh]
+ -l is the twitter login
+ -c is the number of tweets to download 
+ -f is the output file for serialized php data
+gettweets.pl gets and parses twitter content and saves it in a
+way that is easy for a php page to use. 
+gettweets.pl is intended to be run occasionally as a cron job.
+TXT
+print $options and exit if $opt{h};
 
 my $deflogin = 'lifelinevm';
 my $me = $opt{l} || $deflogin;
@@ -16,6 +26,10 @@ my $count = $opt{c} || 3;
 my $verbose = $opt{v};
 
 my $serialfile = "/usr/local/asterisk/html/lifeline/tweets.serialized";
+if ($opt{f}) { 
+	$serialfile = $opt{f};
+}
+
 my $apiurl = "http://api.twitter.com/statuses/user_timeline.json?screen_name=$me&count=$count";
 print STDERR $apiurl,"\n" if $verbose;
 
