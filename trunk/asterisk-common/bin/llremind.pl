ab393d3d030cbfb57707b3252bb7bd24de35ef22
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-10-09 18:41:22 +0000

    modified llremind*.pl so that they send phone number along with box when sending a message added check for boxes in coolaid database

--- trunk/asterisk-common/bin/llremind.pl
+++ trunk/asterisk-common/bin/llremind.pl
@@ -13,22 +13,27 @@ my $optoutdir = '/vservers/callbackpack10/bin/llremind/optout';
 my %emails;
 while (<>) {
 	chomp;
-	my ($box, $email) = split;
+	my ($box, $email, @llphone) = split;
+	my $llphone = join " ", @llphone;
 	next unless $box =~ /^\d+$/;
 	next unless $email =~ /^\w[\w\-\.]*\@\w[\w\-\.]*\.\w{2,4}$/;
-	push @{$emails{$email}}, $box;
+	next unless $llphone =~ /^[\w\-\.\(\) ]{0,20}$/;
+	push @{$emails{$email}}, [$llphone, $box];
 }
 
 foreach my $email (keys %emails) {
-	print "$email has opted out\n" and next if -f "$optoutdir/".md5_hex($email); 
-	
-	print "sending reminders for box(es) @{$emails{$email}} to $email\n" if $opt{v};
-	open MAIL, "| $mail -a 'from: $from' -s 'new voice mail for @{$emails{$email}}' $email" 
-		or die "can't open $mail: $!";
-	print MAIL <<TXT;
+	foreach my $edata (@{$emails{$email}}) {
+		my ($llphone,$box) = @$edata;
+		print "$email has opted out\n" and next if -f "$optoutdir/".md5_hex($email); 
+		
+		my $vphone = "$llphone ext $box";
+		print "sending reminders for $vphone to $email\n" if $opt{v};
+		open MAIL, "| $mail -a 'from: $from' -s 'new voice mail for $vphone' $email" 
+			or die "can't open $mail: $!";
+		print MAIL <<TXT;
 DO NOT REPLY TO THIS EMAIL MESSAGE.
 
-You have new voice mail messages.
+You have new voice mail messages in $vphone.
 
 Lifeline Voice Mail
 http://callbackpack.com
@@ -37,7 +42,8 @@ http://callbackpack.com
 Opt out: http://callbackpack.com/optout.php
 
 TXT
-	close MAIL;
-	sleep(2);
+		close MAIL;
+		sleep(2);
+	}
 }
 
--- trunk/asterisk-common/bin/llremindinit.pl
+++ trunk/asterisk-common/bin/llremindinit.pl
@@ -8,30 +8,28 @@ my $sendhost = 'callbackpack.com';
 my $sendapp = '/vservers/callbackpack10/bin/llremind.pl -v';
 
 my $get = $ldb->prepare(
-	"select box,email ".
+	"select box,email,llphone,'lifeline.boxes' ".
 	"from boxes ".
 	"where new_msgs=1 and reminder=0 ".
 	"and email <> '' and email is not null ".
 	"and paidto >= current_date()".
 	"union ".
-	"select box,email ".
+	"select box,email,llphone,'coolaid.boxes' ".
 	"from coolaid.boxes ".
 	"where new_msgs=1 and reminder=0 ".
 	"and email <> '' and email is not null ".
 	"and paidto >= current_date()"
 );
 
-my $upd = $ldb->prepare("update boxes set reminder=1 where box=?");
-
 $get->execute or $get->errstr;
 
 my $send;
 while (my $row = $get->fetch) {
-	my ($box,$email) = @$row;
-	$upd->execute($box) or die $upd->errstr;
+	my ($box,$email,$llphone,$table) = @$row;
+	$ldb->do("update $table set reminder=1 where box='$box'") or die $ldb->errstr;
 	warn "non-numeric box $box!" and next unless $box =~ /^\d+$/;
 	warn "invalid email $email!" and next unless $email =~ /^\w[\w\.\-]*\@\w[\w\.\-]*\.\w{2,4}$/;
-	$send .= "$box $email\n";
+	$send .= "$box $email $llphone\n";
 }
 $get->finish;
 
