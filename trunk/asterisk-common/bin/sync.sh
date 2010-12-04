2089142e4224f183bc9eacc19ea5ada6d9e1144b
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2010-12-04 15:48:15 +0000

    removed part of cleanup.pl that deleted messages where there was a deleted version of that message removed some unused lines from sync.sh

--- trunk/asterisk-common/bin/sync.sh
+++ trunk/asterisk-common/bin/sync.sh
@@ -6,10 +6,7 @@ echo updating the svn repository on lifelinevm.net
 /usr/bin/rsync --delete -rcvt asterisk@lifelinevm.net:/usr/local/asterisk/paycode/ /usr/local/asterisk/paycode
 /usr/bin/rsync --delete --exclude='database' -rcvt asterisk@lifelinevm.net:/usr/local/asterisk/agi-bin/ /usr/local/asterisk/agi-bin
 /usr/bin/rsync -rcvt --exclude='*.out' asterisk@lifelinevm.net:/usr/local/asterisk/bin/ /usr/local/asterisk/bin
-# /usr/bin/rsync --delete -rcvt asterisk@lifelinevm.net:/usr/local/asterisk/Lifeline/ /usr/local/asterisk/Lifeline
 /usr/bin/rsync --delete -rcvt asterisk@lifelinevm.net:/usr/local/asterisk/dbbackups/ /usr/local/asterisk/dbbackups
-# see sync-msgs.sh
-# /usr/bin/rsync -rcvt asterisk@lifelinevm.net:/usr/local/asterisk/lifeline-msgs/ /usr/local/asterisk/lifeline-msgs
 # echo cleanup `/bin/date`
 # /usr/local/asterisk/bin/cleanup.pl -v
 echo updating the svn repository
