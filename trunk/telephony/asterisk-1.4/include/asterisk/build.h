4d0d3bf7f508050549e9191129b3c7d9fe4fc115
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2011-01-29 14:22:07 +0000

    adding in new files from new asterisk build

---
+++ trunk/telephony/asterisk-1.4/include/asterisk/build.h
@@ -0,0 +1,11 @@
+/*
+ * build.h 
+ * Automatically generated
+ */
+#define BUILD_HOSTNAME "drdata.dyndns.org"
+#define BUILD_KERNEL "2.6.32-27-generic-pae"
+#define BUILD_MACHINE "i686"
+#define BUILD_OS "Linux"
+#define BUILD_DATE "2011-01-22 18:35:06 UTC"
+#define BUILD_USER "asterisk"
+
