180b5da7c79626f0d387dc1fff5aec78b7e3d9fe
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2011-01-29 14:22:07 +0000

    adding in new files from new asterisk build

---
+++ telephony/asterisk-1.4/defaults.h
@@ -0,0 +1,23 @@
+/*
+ * defaults.h 
+ * Automatically generated
+ */
+#define AST_CONFIG_DIR "/usr/local/asterisk-1.4/etc/asterisk"
+#define AST_RUN_DIR    "/usr/local/asterisk-1.4/var/run"
+#define AST_SOCKET     "/usr/local/asterisk-1.4/var/run/asterisk.ctl"
+#define AST_PID        "/usr/local/asterisk-1.4/var/run/asterisk.pid"
+#define AST_MODULE_DIR "/usr/local/asterisk-1.4/lib/asterisk/modules"
+#define AST_SPOOL_DIR  "/usr/local/asterisk-1.4/var/spool/asterisk"
+#define AST_VAR_DIR    "/usr/local/asterisk-1.4/var/lib/asterisk"
+#define AST_DATA_DIR   "/usr/local/asterisk-1.4/var/lib/asterisk"
+#define AST_LOG_DIR    "/usr/local/asterisk-1.4/var/log/asterisk"
+#define AST_AGI_DIR    "/usr/local/asterisk-1.4/var/lib/asterisk/agi-bin"
+#define AST_KEY_DIR    "/usr/local/asterisk-1.4/var/lib/asterisk/keys"
+#define AST_DB         "/usr/local/asterisk-1.4/var/lib/asterisk/astdb"
+#define AST_TMP_DIR    "/usr/local/asterisk-1.4/var/spool/asterisk/tmp"
+
+#define AST_CONFIG_FILE "/usr/local/asterisk-1.4/etc/asterisk/asterisk.conf"
+
+#define AST_SOUNDS     "/usr/local/asterisk-1.4/var/lib/asterisk/sounds"
+#define AST_IMAGES     "/usr/local/asterisk-1.4/var/lib/asterisk/images"
+
---
+++ telephony/asterisk-1.4/include/asterisk/version.h
@@ -0,0 +1,7 @@
+/*
+ * version.h 
+ * Automatically generated
+ */
+#define ASTERISK_VERSION "SVN--r286"
+#define ASTERISK_VERSION_NUM 999999
+
