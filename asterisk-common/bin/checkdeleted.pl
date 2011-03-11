1ab772dfc146d272a4cd1bb3fd8d4d5e2a88894d
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2011-03-11 02:47:15 +0000

    added script to check for undeleted messages

--- asterisk-common/bin/checkdeleted.pl
+++ asterisk-common/bin/checkdeleted.pl
@@ -3,10 +3,12 @@
 # check that their messages are deleted
 use Lifeline::DB;
 use File::Find;
+use File::Copy;
 use Getopt::Std;
 use strict;
 my %opt;
-getopts("v",\%opt);
+getopts("vd",\%opt);
+print "use -d to hide messages automatically, -v verbose\n" if $opt{v};
 
 my $who = shift;
 chomp $who;
@@ -24,7 +26,7 @@ $get->execute or die $get->errstr;
 while (my $row = $get->fetch) {
 	my ($box, $status, $paidto) = @$row;
 	my $srchdir = "$basepath/$box";
-	print "checking $box (status $status, paidto $paidto) $srchdir\n" if $opt{v};
+	print scalar(localtime),": checking $box (status $status, paidto $paidto) $srchdir\n" if $opt{v};
 	warn "$srchdir not a directory!" unless $srchdir;
 	finddepth(\&wanted, $srchdir);
 }
@@ -33,6 +35,13 @@ $get->finish;
 
 sub wanted {
 	return unless /\.gsm$/;
-	print "NOT DELETED: $File::Find::name\n" unless /deleted\.gsm$/;
+	unless (/deleted\.gsm$/) {
+		print scalar(localtime),": NOT DELETED: $File::Find::name\n";
+		if ($opt{d} and $_ !~ /deleted\.gsm$/) {
+			print scalar(localtime),": hiding message $File::Find::name\n";
+			(my $new = $_) =~ s/(.*)(\.gsm)/$1.deleted$2/;
+			move $_, $new or die "can't move $_ to $new: $!";
+		}
+	}
 }
 
