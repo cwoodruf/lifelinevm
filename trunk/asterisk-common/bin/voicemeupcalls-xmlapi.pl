5b7c9188b49a81323e20bc553d32bd24230ab5a1
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2011-04-25 20:44:27 +0000

    modified call import script to monitor error conditions

--- trunk/asterisk-common/bin/voicemeupcalls-xmlapi.pl
+++ trunk/asterisk-common/bin/voicemeupcalls-xmlapi.pl
@@ -5,23 +5,47 @@ use Data::Dumper;
 use Getopt::Std;
 use strict;
 my %opt;
-getopts('v', \%opt);
+getopts('vm:t:', \%opt);
 
-my $auth_token = '8fa0b3a8d3029ed9fecf0e35071d7842';
+my $auth_token = 'add token here';
 my $client_account_id = '80015026';
 my $baseurl = "http://developer.voicemeup.com/xml_engine/xml_engine.php";
 my $auth = "result_limit=1000&client_account_id=$client_account_id&auth_token=$auth_token";
 
-my $day = shift;
-$day = `/bin/date +\%Y-\%m-01` unless $day =~ /^\d\d\d\d-\d\d-\d\d/;
-my $endday = shift;
-my $date_to = "&date_to=$endday" if $endday =~ /^\d\d\d\d-\d\d-\d\d/;
-my $url = "$baseurl?$auth&request_type=get_calls&date_from=$day$date_to";
+my ($day,$endday,$date_to,$url,$unique_id,$err);
+$day = shift;
+if ($day ne '') {
+	$endday = shift;
+	$date_to = "&date_to=$endday" if $endday =~ /^\d\d\d\d-\d\d-\d\d/;
+	$url = "$baseurl?$auth&request_type=get_calls&date_from=$day$date_to";
+} else {
+	my $get_last_ts = $ldb->prepare("select max(ts) from voicemeupcalls");
+	if ($get_last_ts->execute) {
+		if ($get_last_ts->rows) {
+			my $last_ts = ($get_last_ts->fetch)->[0];
+			my $get_last_call = $ldb->prepare(
+				"select * from voicemeupcalls where ts='$last_ts'"
+			);
+			if ($get_last_call->execute) {
+				my $last_call = $get_last_call->fetchrow_hashref;
+				if (defined $last_call) {
+					$unique_id = $last_call->{unique_id};
+					$url = "$baseurl?$auth&request_type=get_calls&unique_id=$unique_id";
+				}
+			} 
+		}
+	}
+	unless (defined $unique_id) {
+		warn "couldn't get last call - using date instead";
+		$day = `/bin/date +\%Y-\%m-\%d`;
+		$url = "$baseurl?$auth&request_type=get_calls&date_from=$day";
+	}
+}
 print "$url\n" if $opt{v};
 my $xml = get($url);
 
 die "error getting $xml: $!" unless defined $xml;
-print "$xml\n" if $opt{v};
+# print "$xml\n" if $opt{v};
 
 my @calls;
 
@@ -71,9 +95,8 @@ my @fields = qw/
 	current_rate
 	monthly_usage
 /;
-my $insq = "replace into voicemeupcalls (unique_id,".(join ",", @fields).") ".
-		"values (?,".(join ",", map {"?"} @fields).")";
-my $ins = $ldb->prepare($insq);
+my ($count,$notanswered);
+$count = $notanswered = 0;
 while ($xml =~ m#<call_details unique_id="(.*?)">(.*?)</call_details>#sg) {
 	my $id = $1;
 	my $call = $2;
@@ -83,5 +106,26 @@ while ($xml =~ m#<call_details unique_id="(.*?)">(.*?)</call_details>#sg) {
 			$rec{$field} = $1;
 		}
 	}
-	$ins->execute($id,@rec{@fields}) or die "call:\n".Dumper(\%rec).$ins->errstr;
+	$count++;
+	$notanswered++ unless $rec{status} eq 'answered';
+	my $insq = "insert ignore into voicemeupcalls (unique_id,".(join ",", @fields).",ts) ".
+			"values (".
+				$ldb->quote($id).",".
+				(join ",", map {$ldb->quote($_)} @rec{@fields}).",".
+				$ldb->quote("$rec{date} $rec{'time'}").
+			")";
+	print "$insq\n" if $opt{v};
+	my $ins = $ldb->prepare($insq);
+	$ins->execute or die $ins->errstr;
 }
+print scalar(localtime),": notanswered $notanswered, count $count, threshold $opt{t}, email $opt{m} ";
+if ($opt{m} =~ /\@/ and defined $opt{t}) {
+	if ($notanswered >= $opt{t}) {
+		print "sending email";
+		open MAIL, "| /usr/bin/mail -s 'unanswered calls' '$opt{m}'";
+		print MAIL "unanswered $notanswered, total $count";
+		close MAIL or die "error sending mail to $opt{m}: $!";
+	}
+}
+print "\n";
+
