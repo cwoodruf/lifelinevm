8b0bf50f02f1abc9bd8c57b433c4981fbe3b9507
cwoodruf@gmail.com <cwoodruf@gmail.com@c4979def-857e-07ba-381f-816fb6ae246e>
2011-07-31 04:28:02 +0000

    asterisk upgrade

--- trunk/telephony/asterisk-1.4-20110730/include/asterisk/version.h
+++
@@ -1,7 +0,0 @@
-/*
- * version.h 
- * Automatically generated
- */
-#define ASTERISK_VERSION "SVN--r292M"
-#define ASTERISK_VERSION_NUM 999999
-
--- trunk/telephony/asterisk-1.6-20110730/include/asterisk/version.h
+++
@@ -1,7 +0,0 @@
-/*
- * version.h 
- * Automatically generated
- */
-#define ASTERISK_VERSION "SVN--r296M"
-#define ASTERISK_VERSION_NUM 999999
-
